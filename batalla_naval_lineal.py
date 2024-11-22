9bd59d968395cf6623282cc4dabaf971c150ffa2
Juan Dalmau <drjuanma2712@gmail.com>
2024-11-22 00:11:02 -0300

    avances

--- batalla_naval_lineal.py
+++ batalla_naval_lineal.py
@@ -1,6 +1,6 @@
 import pulp
 from pulp import LpAffineExpression as Sum
-
+"""
 def adyacentes(matriz, fila, columna, direcciones):
     
     elementos = []
@@ -128,9 +128,137 @@ def batalla_naval_lineal(demandas_col: list, demandas_fil: list, long_barcos: li
                     solucion[f][c] = 1
     
     return solucion
+"""
 
 
-l_barcos = [3,1,1,2,4,2,1,2,1,3]
-restricciones_filas = [3,2,2,4,2,1,1,2,3,0]
-restricciones_columnas = [1,2,1,3,2,2,3,1,5,0]
-print(batalla_naval_lineal(restricciones_columnas,restricciones_filas,l_barcos))
+def batalla_naval_lineal2(demandas_col: list, demandas_fil: list, long_barcos: list):
+    
+    problem = pulp.LpProblem("bn", pulp.LpMaximize)
+
+    barcos_vars = []
+    for b in range(len(long_barcos)):
+        vars = {
+            "O": pulp.LpVariable("o"+str((b)),cat="Binary"),
+            "B": pulp.LpVariable("b"+str((b)),cat="Binary"),
+            "C": pulp.LpVariable("c"+str((b)),cat="Integer"),
+            "F": pulp.LpVariable("f"+str((b)),cat="Integer"),
+            "2A": [pulp.LpVariable("2a"+str((b,b2)),cat="Binary") for b2 in range(len(long_barcos))],
+            "2D": [pulp.LpVariable("2d"+str((b,b2)),cat="Binary") for b2 in range(len(long_barcos))],
+            "InfBCol": [pulp.LpVariable("infBcol"+str((b,c)),cat="Binary") for c in range(len(demandas_col))],
+            "SupBCol": [pulp.LpVariable("supBcol"+str((b,c)),cat="Binary") for c in range(len(demandas_col))],
+            "BCol": [pulp.LpVariable("Bcol"+str((b,c)),cat="Binary") for c in range(len(demandas_col))],
+            "ICol": [pulp.LpVariable("Icol"+str((b,c)),cat="Integer") for c in range(len(demandas_col))],
+            "InfBFil": [pulp.LpVariable("infBfil"+str((b,f)),cat="Binary") for f in range(len(demandas_fil))],
+            "SupBFil": [pulp.LpVariable("supBfil"+str((b,f)),cat="Binary") for f in range(len(demandas_fil))],
+            "BFil": [pulp.LpVariable("Bfil"+str((b,f)),cat="Binary") for f in range(len(demandas_fil))],
+            "IFil": [pulp.LpVariable("IFil"+str((b,f)),cat="Integer") for f in range(len(demandas_fil))]
+        }
+        barcos_vars.append(vars)
+    
+    demandas_cumplidas_columnas = [pulp.LpVariable("dcc"+str((c)),cat="Integer") for c in range(len(demandas_col))]
+    demandas_cumplidas_filas = [pulp.LpVariable("dcf"+str((f)),cat="Integer") for f in range(len(demandas_fil))]
+    bcols = [[] for col in range(len(demandas_col))]
+    bfils = [[] for fil in range(len(demandas_fil))]
+
+    M = len(demandas_col)*len(demandas_fil)
+    # Ahora las restricciones
+    for i in range(len(barcos_vars)):
+        
+        actual = barcos_vars[i]
+
+        # Limites para que no se salga del tablero
+        problem += actual["F"] >= 0
+        problem += actual["F"] >= len(demandas_fil)-1
+        problem += actual["C"] >= 0
+        problem += actual["C"] >= len(demandas_col)-1
+
+        # Para que entre todo el barco en el tablero
+        problem += actual["F"] + long_barcos[i] <= len(demandas_fil) + M * (1 - actual["O"])
+        problem += actual["C"] + long_barcos[i] <= len(demandas_fil) + M * (actual["O"])
+
+        # Ahora tengo que hacer que los barcos se respeten entre si
+        for j in range(len(barcos_vars)):
+            if j == i:
+                continue
+            aux = barcos_vars[j]
+
+            # Fuerzo A2 y D2
+            problem += actual["F"] >= aux["F"] + (long_barcos[j] - 1) * aux["O"] + 2 - M * (1 - actual["2A"][j])
+            problem += actual["F"] <= aux["F"] + (long_barcos[j] - 1) * aux["O"] + 1 + M * (actual["2A"][j])
+            problem += actual["C"] >= aux["C"] + (long_barcos[j] - 1) * (1 - aux["O"]) + 2 - M * (1 - actual["2D"][j])
+            problem += actual["C"] <= aux["C"] + (long_barcos[j] - 1) * (1 - aux["O"]) + 1 + M * (actual["2D"][j])
+
+            problem += actual["2D"][j] + actual["2A"][j] + aux["2D"][i] + aux["2A"][i] >= 2 - M * (1 - actual["B"]) - M * (1 - aux["B"])
+        
+        # Falta ver las demandas
+        for c in range(len(demandas_col)):
+
+            # Fuerzo infBcol para que me diga si el barco empieza antes de la columna c o despues
+            problem += actual["C"] <= c + M * (1 - actual["InfBCol"][c])
+            problem += actual["C"] >= c + 1 - M * (actual["InfBCol"][c])
+            
+            # Fuerzo supBcol para que me diga si el barco termina antes de la columna c o justo en c
+            problem += actual["C"] + (long_barcos[i] - 1) * (1 - actual["O"]) >= c - M * (actual["SupBCol"][c])
+            problem += actual["C"] + (long_barcos[i] - 1) * (1 - actual["O"]) <= c - 1 + M * (1 - actual["SupBCol"][c])
+
+            # Consigo Bcol par ver si este barco afecta el tablero final
+            problem += actual["B"] + actual["InfBCol"][c] + actual["SupBCol"][c] >= 3 - M * (1 - actual["BCol"][c])
+            problem += actual["B"] + actual["InfBCol"][c] + actual["SupBCol"][c] <= 2 + M * (actual["BCol"][c])
+        
+            problem += actual["ICol"] == actual["BCol"][c] + (long_barcos[i] - 1) * (1 - actual["O"])
+            bcols[c].append(actual["ICol"])
+        for f in range(len(demandas_fil)):
+
+            # Fuerzo infBcol para que me diga si el barco empieza antes de la columna c o despues
+            problem += actual["F"] <= f + M * (1 - actual["InfBFil"][f])
+            problem += actual["F"] >= f + 1 - M * (actual["InfBFil"][f])
+            
+            # Fuerzo supBcol para que me diga si el barco termina antes de la columna c o justo en c
+            problem += actual["F"] + (long_barcos[i] - 1) * (actual["O"]) >= f - M * (actual["SupBFil"][f])
+            problem += actual["F"] + (long_barcos[i] - 1) * (actual["O"]) <= f - 1 + M * (1 - actual["SupBFil"][f])
+
+            # Consigo Bcol par ver si este barco afecta el tablero final
+            problem += actual["B"] + actual["InfBFil"][f] + actual["SupBFil"][f] >= 3 - M * (1 - actual["BFil"][f])
+            problem += actual["B"] + actual["InfBFil"][f] + actual["SupBFil"][f] <= 2 + M * (actual["BFil"][f])
+
+            problem += actual["IFil"] == actual["BFil"][f] + (long_barcos[i] - 1) * (actual["O"])
+            bcols[c].append(actual["IFil"])
+        
+    # Obtengo la demanda cumplida y le pongo tope
+    for col in range(len(demandas_cumplidas_columnas)):
+        problem += demandas_cumplidas_columnas[col] == Sum([(dem,1) for dem in bcols[col]])
+        problem += demandas_cumplidas_columnas[col] <= demandas_col[col]
+    
+    for fil in range(len(demandas_cumplidas_filas)):
+        problem += demandas_cumplidas_filas[fil] == Sum([(dem,1) for dem in bfils[fil]])
+        problem += demandas_cumplidas_filas[fil] <= demandas_fil[fil]
+    
+    # Y maximizo la suma de las demandas
+    problem += Sum([(dem,1) for dem in demandas_cumplidas_columnas] + [(dem,1) for dem in demandas_cumplidas_filas])
+
+    # Resuelvo
+    problem.writeLP("modelo.lp")
+    problem.solve()
+    problem.writeLP("modelo_reducido.lp")
+
+    # Interpreto resultados
+    solucion = [[0 for col in range(len(demandas_col))] for fil in range(len(demandas_fil))]
+
+    for b in range(len(barcos_vars)):
+        barco = barcos_vars[b]
+        if int(pulp.value(barco["B"])) == 1:
+            f, c, o = int(pulp.value(barco["F"])), int(pulp.value(barco["C"])), int(pulp.value(barco["O"]))
+            for _ in range(long_barcos[b]):
+                solucion[f][c] = 1
+                if o == 1:
+                    f += 1
+                else:
+                    c += 1
+    
+    return solucion
+
+
+l_barcos = [3,1,1,2,4,2]
+restricciones_filas = [3,2,2,4,3,0]
+restricciones_columnas = [2,2,3,1,5,0]
+print(batalla_naval_lineal2(restricciones_columnas,restricciones_filas,l_barcos))
